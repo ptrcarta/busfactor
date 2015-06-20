229a84742deecf704ede3faaf52cb2fd382b86d7
ptrcarta <ptrcarta@gmail.com>
2015-06-20 02:01:41 +0200

    resume from downloaded stats

--- get_commit_stats.py
+++ get_commit_stats.py
@@ -9,17 +9,19 @@ search_files = os.listdir('popular_repos')
 search_files = list(zip(*sorted(map(lambda x: (int(x.split('_')[-1].split('.')[0]), x), search_files))))[1]
 
 
-repos = list()
+repos = set()
 for sf in search_files:
     with open('popular_repos/'+sf) as f:
         search_res = json.load(f)
         for r in search_res['items']:
-            repos.append((r['full_name'], r['stargazers_count']))
+            repos.add(r['full_name'])
 
-for repo in repos:
+done_repos = set(map(lambda x: x.replace('?', '/'), os.listdir('repos_stats')))
+
+for repo in repos - done_repos:
     r = requests.get(STATS_URL.format(fullname=repo[0]))
     if r.status_code == 202:
-        time.sleep(0.5)
+        time.sleep(6)
         r = requests.get(STATS_URL.format(fullname=repo[0]))
     if r.status_code == 200:
         print(repo[0])
